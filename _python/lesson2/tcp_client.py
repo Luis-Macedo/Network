f4df05ff98177f61eae3fd671c1d6b121706d8e6
= <luismacedo2001@outlook.com>
2022-02-10 22:28:43 -0300

    tcp done

---
+++ _python/lesson2/tcp_client.py
@@ -0,0 +1,59 @@
+import socket
+from xml.dom.pulldom import parseString
+
+class Client:
+
+    def __init__(self, tcp_port, tcp_ip, buf_size):
+
+        self.tcp_port = tcp_port,
+        self.tcp_ip = tcp_ip,
+        self.buf_size = buf_size,
+        self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    def getTcp_ip(self):
+        return self.tcp_ip[0]
+
+    def getTcp_port(self):
+        return self.tcp_port[0]
+    
+    def getBuf_size(self):
+        return self.buf_size[0]
+
+    def connectSocket(self):
+
+        tcp_ip = self.getTcp_ip()
+        tcp_port = self.getTcp_port()
+        buf_size = self.getBuf_size()
+        tcp_socket = self.tcp_socket
+
+        print("[INFO] Conectando socket com a porta: ",tcp_port)
+        tcp_socket.connect((tcp_ip,tcp_port))
+        print("[INFO] Socket conectado à porta: ",tcp_port)
+
+        msg = "Enviando um olá"
+        msg = msg.encode('utf-8')
+
+        print("[INFO] Enviando dados para o servidor...")
+        tcp_socket.send(msg)
+        print("[INFO] Dados enviados com sucesso!")
+
+        print("[INFO] Recebendo dados do servidor...")
+        data = tcp_socket.recv(buf_size)
+
+        print("[INFO] Decodificando dados recbidos...")
+        data = data.decode('utf-8')
+
+        print('[INFO] Dados recbidos do servidor:',data)
+
+
+        print("[INFO] Desconectando socket...")
+        tcp_socket.close()
+        print("[INFO] Socket desconectado com sucesso!")
+
+tcp_port = 8000
+tcp_ip = '127.0.0.1'
+buf_size = 30
+
+client = Client(tcp_port, tcp_ip, buf_size)
+client.connectSocket()
+
---
+++ _python/lesson2/tcp_server.py
@@ -0,0 +1,71 @@
+import socket
+
+class Server:
+
+    def __init__(self, tcp_port, tcp_ip, buf_size):
+
+        self.tcp_port = tcp_port,
+        self.tcp_ip = tcp_ip,
+        self.buf_size = buf_size,
+        self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    def getTcp_ip(self):
+        return self.tcp_ip[0]
+
+    def getTcp_port(self):
+        return self.tcp_port[0]
+    
+    def getBuf_size(self):
+        return self.buf_size[0]
+
+    def createSocket(self):
+
+        tcp_ip = self.getTcp_ip()
+        tcp_port = self.getTcp_port()
+        
+        print("[INFO] Socket criado!")
+
+        self.tcp_socket.bind((tcp_ip,tcp_port))
+        print("[INFO] Socket está ligado à porta:",tcp_port)
+
+        self.tcp_socket.listen(1)
+        print("[INFO] Socket está escutando")
+
+    def recieve(self):
+        
+        tcp_socket = self.tcp_socket
+        client, address = tcp_socket.accept()
+        buf_size = self.getBuf_size()
+
+        print("[INFO] Endereço de conexão vindo de: ",address)
+
+        print("[INFO] Recebendo dados do cliente...")
+        data = client.recv(buf_size)
+
+        print("[INFO] Decodificando dados do cliente...")
+        data = data.decode('utf-8')
+
+        print("[INFO] Ddaos recebidos do cliente:",data)
+
+        msg = "Até mais!"
+        msg = msg.encode('utf-8')
+
+        print("[INFO] Enviando dados para o cliente...")
+        client.send(msg)
+        print("[INFO] Dados enviados com sucesso")
+
+        print("[INFO] Desconectando o client...")
+        client.close()
+
+        print("[INFO] Desconectando socket...")
+        tcp_socket.close()
+        print("[INFO] Socket desconectado!")
+
+tcp_port = 8000
+tcp_ip = '127.0.0.1'
+buf_size = 30
+
+server = Server(tcp_port, tcp_ip, buf_size)
+
+server.createSocket()
+server.recieve()
